ef9a75e1b39605d146d6f4c3dfd3ddda245ec491
flux <flux@ns547581.ip-66-70-181.net>
2019-07-25 02:33:19 +0000

    diffs for irc2

--- init.lua
+++ init.lua
@@ -1,38 +1,38 @@
 
-local irc_users = {}
+local irc2_users = {}
 
 local old_chat_send_player = minetest.chat_send_player
 minetest.chat_send_player = function(name, message) -- luacheck: ignore
-	for nick, loggedInAs in pairs(irc_users) do
+	for nick, loggedInAs in pairs(irc2_users) do
 		if name == loggedInAs and not minetest.get_player_by_name(name) then
-			irc:say(nick, message)
+			irc2:say(nick, message)
 		end
 	end
 	return old_chat_send_player(name, message)
 end
 
-irc.register_hook("NickChange", function(user, newNick)
-	for nick, player in pairs(irc_users) do
+irc2.register_hook("NickChange", function(user, newNick)
+	for nick, player in pairs(irc2_users) do
 		if nick == user.nick then
-			irc_users[newNick] = irc_users[user.nick]
-			irc_users[user.nick] = nil
+			irc2_users[newNick] = irc2_users[user.nick]
+			irc2_users[user.nick] = nil
 		end
 	end
 end)
 
-irc.register_hook("OnPart", function(user, channel, reason)
-	irc_users[user.nick] = nil
+irc2.register_hook("OnPart", function(user, channel, reason)
+	irc2_users[user.nick] = nil
 end)
 
-irc.register_hook("OnKick", function(user, channel, target, reason)
-	irc_users[target] = nil
+irc2.register_hook("OnKick", function(user, channel, target, reason)
+	irc2_users[target] = nil
 end)
 
-irc.register_hook("OnQuit", function(user, reason)
-	irc_users[user.nick] = nil
+irc2.register_hook("OnQuit", function(user, reason)
+	irc2_users[user.nick] = nil
 end)
 
-irc.register_bot_command("login", {
+irc2.register_bot_command("login", {
 	params = "<username> <password>",
 	description = "Login as a user to run commands",
 	func = function(user, args)
@@ -44,7 +44,7 @@ irc.register_bot_command("login", {
 			return false, "Player name and password required."
 		end
 		local inChannel = false
-		local channel = irc.conn.channels[irc.config.channel]
+		local channel = irc2.conn.channels[irc2.config.channel]
 		if not channel then
 			return false, "The server needs to be in its "..
 				"channel for anyone to log in."
@@ -63,7 +63,7 @@ irc.register_bot_command("login", {
 		if auth and minetest.check_password_entry(playerName, auth.password, password) then
 			minetest.log("action", "User "..user.nick
 					.." from IRC logs in as "..playerName)
-			irc_users[user.nick] = playerName
+			irc2_users[user.nick] = playerName
 			handler.record_login(playerName)
 			return true, "You are now logged in as "..playerName
 		else
@@ -74,13 +74,13 @@ irc.register_bot_command("login", {
 	end
 })
 
-irc.register_bot_command("logout", {
+irc2.register_bot_command("logout", {
 	description = "Logout",
 	func = function (user, args)
-		if irc_users[user.nick] then
+		if irc2_users[user.nick] then
 			minetest.log("action", user.nick.."@IRC logs out from "
-				..irc_users[user.nick])
-			irc_users[user.nick] = nil
+				..irc2_users[user.nick])
+			irc2_users[user.nick] = nil
 			return true, "You are now logged off."
 		else
 			return false, "You are not logged in."
@@ -88,14 +88,14 @@ irc.register_bot_command("logout", {
 	end,
 })
 
-irc.register_bot_command("cmd", {
+irc2.register_bot_command("cmd", {
 	params = "<command>",
 	description = "Run a command on the server",
 	func = function (user, args)
 		if args == "" then
 			return false, "You need a command."
 		end
-		if not irc_users[user.nick] then
+		if not irc2_users[user.nick] then
 			return false, "You are not logged in."
 		end
 		local found, _, commandname, params = args:find("^([^%s]+)%s(.+)$")
@@ -106,34 +106,34 @@ irc.register_bot_command("cmd", {
 		if not command then
 			return false, "Not a valid command."
 		end
-		if not minetest.check_player_privs(irc_users[user.nick], command.privs) then
+		if not minetest.check_player_privs(irc2_users[user.nick], command.privs) then
 			return false, "Your privileges are insufficient."
 		end
 		minetest.log("action", user.nick.."@IRC runs "
-			..args.." as "..irc_users[user.nick])
-		return command.func(irc_users[user.nick], (params or ""))
+			..args.." as "..irc2_users[user.nick])
+		return command.func(irc2_users[user.nick], (params or ""))
 	end
 })
 
-irc.register_bot_command("say", {
+irc2.register_bot_command("say", {
 	params = "message",
 	description = "Say something",
 	func = function (user, args)
 		if args == "" then
 			return false, "You need a message."
 		end
-		if not irc_users[user.nick] then
+		if not irc2_users[user.nick] then
 			return false, "You are not logged in."
 		end
-		if not minetest.check_player_privs(irc_users[user.nick], {shout=true}) then
+		if not minetest.check_player_privs(irc2_users[user.nick], {shout=true}) then
 			minetest.log("action", ("%s@IRC tried to say %q as %s"
 				.." without the shout privilege.")
-					:format(user.nick, args, irc_users[user.nick]))
+					:format(user.nick, args, irc2_users[user.nick]))
 			return false, "You can not shout."
 		end
 		minetest.log("action", ("%s@IRC says %q as %s.")
-				:format(user.nick, args, irc_users[user.nick]))
-		minetest.chat_send_all("<"..irc_users[user.nick].."@IRC> "..args)
+				:format(user.nick, args, irc2_users[user.nick]))
+		minetest.chat_send_all("<"..irc2_users[user.nick].."@IRC> "..args)
 		return true, "Message sent successfuly."
 	end
 })
